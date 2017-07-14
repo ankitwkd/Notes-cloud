c8b31f8e6979d4b7edc7c8b8fbc501a71437419d
root <wkd.ankit97@gmail.com>
2017-07-14 15:08:27 +0530

    First

---
+++ app/src/main/java/com/wkdnotes/root/notes_cloud/UploadActivity.java
@@ -0,0 +1,143 @@
+package com.wkdnotes.root.notes_cloud;
+
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.provider.MediaStore;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.OnProgressListener;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+
+public class UploadActivity extends AppCompatActivity implements View.OnClickListener /*  implementing click listener */ {
+    //a constant to track the file chooser intent
+    private static final int PICK_FILE_REQUEST = 234;
+
+    //Buttons
+    private Button buttonChoose;
+    private Button buttonUpload;
+    private StorageReference mStorageRef;
+
+
+
+    //a Uri object to store file path
+    private Uri filePath;
+    private TextView tv;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_upload);
+
+        //getting views from layout
+        buttonChoose = (Button) findViewById(R.id.choose);
+        buttonUpload = (Button) findViewById(R.id.upload);
+
+        tv=(TextView)findViewById(R.id.textView);
+
+        //attaching listener
+        buttonChoose.setOnClickListener(this);
+        buttonUpload.setOnClickListener(this);
+        mStorageRef = FirebaseStorage.getInstance().getReference();
+    }
+
+    //method to show file chooser
+    private void showFileChooser() {
+        Intent intent = new Intent();
+        intent.setType("application/pdf");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(Intent.createChooser(intent, "Select Document"), PICK_FILE_REQUEST);
+    }
+
+    //handling the image chooser activity result
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == PICK_FILE_REQUEST && resultCode == RESULT_OK && data != null && data.getData() != null) {
+            filePath = data.getData();
+
+
+                String filename=filePath.getLastPathSegment();
+                tv.setText(filename);
+
+
+        }
+    }
+    //this method will upload the file
+    private void uploadFile() {
+        //if there is a file to upload
+        if (filePath != null) {
+            //displaying a progress dialog while upload is going on
+            final ProgressDialog progressDialog = new ProgressDialog(this);
+            progressDialog.setTitle("Uploading");
+            progressDialog.show();
+
+            StorageReference riversRef = mStorageRef.child("file/.pdf");
+            riversRef.putFile(filePath)
+                    .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                            //if the upload is successfull
+                            //hiding the progress dialog
+                            progressDialog.dismiss();
+
+                            //and displaying a success toast
+                            Toast.makeText(getApplicationContext(), "File Uploaded ", Toast.LENGTH_LONG).show();
+                        }
+                    })
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception exception) {
+                            //if the upload is not successfull
+                            //hiding the progress dialog
+                            progressDialog.dismiss();
+
+                            //and displaying error message
+                            Toast.makeText(getApplicationContext(), exception.getMessage(), Toast.LENGTH_LONG).show();
+                        }
+                    })
+                    .addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onProgress(UploadTask.TaskSnapshot taskSnapshot) {
+                            //calculating progress percentage
+                            double progress = (100.0 * taskSnapshot.getBytesTransferred()) / taskSnapshot.getTotalByteCount();
+
+                            //displaying percentage in progress dialog
+                            progressDialog.setMessage("Uploaded " + ((int) progress) + "%...");
+                        }
+                    });
+        }
+        //if there is not any file
+        else {
+            //you can display an error toast
+            Toast.makeText(getApplicationContext(),"No file chosen",Toast.LENGTH_SHORT);
+        }
+    }
+
+    @Override
+    public void onClick(View view) {
+        //if the clicked button is choose
+        if (view == buttonChoose) {
+            showFileChooser();
+        }
+        //if the clicked button is upload
+        else if (view == buttonUpload) {
+            uploadFile();
+
+        }
+    }
+}
